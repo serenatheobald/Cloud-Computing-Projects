c68edf6c0a5214b3b7b54773e85f8171f55be714
serenatheo <118126066+serenatheo@users.noreply.github.com>
2023-09-24 15:45:50 -0400

    Updated DS561_HW2_code.py

--- DS561_HW2_code.py
+++ DS561_HW2_code.py
@@ -29,26 +29,24 @@ def build_graph(bucket):
         links = [link.split('"')[1].replace(".html", "") for link in content.split('<a HREF="')[1:]]
         
         file_name = blob.name.split("/")[-1].replace(".html", "")
+        #store the number of outgoing links for each file
         outgoing_links.append(len(links))
         
-        #adding nodes and edges to the graph G
-        #Outgoing Links: Represent the links that a given file is linking out to
-        #Incoming Links: Represent the number of other files that are linking to a given file
-
+        # Adding nodes and edges to the graph G
         G.add_node(file_name)
         for link in links:
+            #add an edge from the current file to the linked file to update graph
             G.add_edge(file_name, link)
-            #check if this link is already recorded in the incoming_links dictionary.
-            #If it is, the count is incremented; otherwise, it's initialized with a value of 1
-            if link in incoming_links:
-                incoming_links[link] += 1
-            else:
-                incoming_links[link] = 1
-
+            
+            # Update incoming links
+            if link not in G:
+                G.add_node(link)
+            incoming_links[link] = len(G.pred[link])
+            
     return G, outgoing_links, incoming_links
 
 #Calculates and prints various statistics
-#regarding outgoing and incoming links, including averages, medians, maxima, minima, and quintiles
+#outgoing and incoming links, including averages, medians, maxima, minima, and quintiles
 def print_statistics(outgoing_links, incoming_links):
     avg_outgoing = np.mean(outgoing_links)
     print("Average outgoing:", avg_outgoing)
@@ -83,36 +81,34 @@ def print_statistics(outgoing_links, incoming_links):
 #Calculates the PageRank of each node in the graph using an iterative method
 #Returns a dictionary with nodes as keys and their corresponding PageRank values as values
 def original_iterative_pagerank(G, damping=0.85, max_iter=1000):
-    N = len(G)  # Find the number of nodes in the graph G
-    pr = {node: 1/N for node in G.nodes()}  # Initialize each node's PageRank to 1/N
-    
-    total_pr_prev = sum(pr.values())  # Sum of all initial PageRank values
+    N = len(G)  # Number of nodes in the graph
+    pr = {node: 1.0/N for node in G.nodes()}  # Initialize each node's PageRank to 1/N
+
+    total_pr_prev = sum(pr.values())  # Initial total PageRank value
 
     for _ in range(max_iter):
-        new_pr = {}  # Hold the new PageRank values for this iteration
-        
+        new_pr = {}
         for node in G.nodes():
-            # For each node, accumulate the PageRank from all its predecessors
-            total_for_node = sum([pr[pred] / len(G[pred]) for pred in G.pred[node]])
-            # we compute the PageRank based on the formula provided
-            new_pr[node] = 0.15 + 0.85 * total_for_node
-
-        pr = new_pr  # Update the PageRank values for the next iteration
+            total_for_node = sum([pr[pred] / len(G[pred]) if G[pred] else 0 for pred in G.pred[node]])
+            
+            # PageRank formula here:
+            new_pr[node] = (1 - damping)/N + damping * total_for_node
         
-        total_pr_new = sum(pr.values())  # Sum of all new PageRank values
-
-        # Iterate until the sum of pageranks across all pages does not change by more than .5% across iterations
-        if abs(total_pr_new - total_pr_prev) / total_pr_prev < 0.005:
+        total_pr_new = sum(new_pr.values())
+        
+        # Check for convergence
+        if abs(total_pr_new - total_pr_prev) / total_pr_prev < 0.005:  # Change is less than 0.5%
             break
-
-        total_pr_prev = total_pr_new
+        
+        pr = new_pr  # Update PageRank values for the next iteration
+        total_pr_prev = total_pr_new  # Update the previous total PageRank value
 
     return pr
 
 def main():
     
     # Set the environment variable for authentication
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/serenatheobald/Downloads/ds-561-first-project-a6047833252d.json"
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/serenatheobald/Downloads/ds-561-first-project-a6047833252d.json"
 
     bucket = initialize_storage_client("serena_ds561_hw2_bucket")
     #construct graph of the pages
